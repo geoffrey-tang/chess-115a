e0f34355aaa6b22c196eca9fb984829c890bf5d5
Aydan <aydansee@gmail.com>
2026-02-11 17:46:06 -0800

    psqt

---
+++ src/engine/psqt.cpp
@@ -0,0 +1,118 @@
+#include "psqt.h"
+#include "constants.h"
+
+static inline int mirror_square(int sq) {
+    return sq ^ 56; // flip vertically for black
+}
+
+/*
+   Tables are written from WHITE perspective.
+   A1 = 0, H8 = 63
+*/
+
+// Pawn
+static constexpr int pawn_table[64] = {
+     0,  0,  0,  0,  0,  0,  0,  0,
+     5, 10, 10,-20,-20, 10, 10,  5,
+     5, -5,-10,  0,  0,-10, -5,  5,
+     0,  0,  0, 20, 20,  0,  0,  0,
+     5,  5, 10, 25, 25, 10,  5,  5,
+    10, 10, 20, 30, 30, 20, 10, 10,
+    50, 50, 50, 50, 50, 50, 50, 50,
+     0,  0,  0,  0,  0,  0,  0,  0
+};
+
+// Knight
+static constexpr int knight_table[64] = {
+    -50,-40,-30,-30,-30,-30,-40,-50,
+    -40,-20,  0,  0,  0,  0,-20,-40,
+    -30,  0, 10, 15, 15, 10,  0,-30,
+    -30,  5, 15, 20, 20, 15,  5,-30,
+    -30,  0, 15, 20, 20, 15,  0,-30,
+    -30,  5, 10, 15, 15, 10,  5,-30,
+    -40,-20,  0,  5,  5,  0,-20,-40,
+    -50,-40,-30,-30,-30,-30,-40,-50
+};
+
+// Bishop
+static constexpr int bishop_table[64] = {
+    -20,-10,-10,-10,-10,-10,-10,-20,
+    -10,  5,  0,  0,  0,  0,  5,-10,
+    -10, 10, 10, 10, 10, 10, 10,-10,
+    -10,  0, 10, 10, 10, 10,  0,-10,
+    -10,  5,  5, 10, 10,  5,  5,-10,
+    -10,  0,  5, 10, 10,  5,  0,-10,
+    -10,  0,  0,  0,  0,  0,  0,-10,
+    -20,-10,-10,-10,-10,-10,-10,-20
+};
+
+// Rook
+static constexpr int rook_table[64] = {
+     0,  0,  5, 10, 10,  5,  0,  0,
+    -5,  0,  0,  0,  0,  0,  0, -5,
+    -5,  0,  0,  0,  0,  0,  0, -5,
+    -5,  0,  0,  0,  0,  0,  0, -5,
+    -5,  0,  0,  0,  0,  0,  0, -5,
+    -5,  0,  0,  0,  0,  0,  0, -5,
+     5, 10, 10, 10, 10, 10, 10,  5,
+     0,  0,  0,  0,  0,  0,  0,  0
+};
+
+// Queen
+static constexpr int queen_table[64] = {
+    -20,-10,-10, -5, -5,-10,-10,-20,
+    -10,  0,  0,  0,  0,  0,  0,-10,
+    -10,  0,  5,  5,  5,  5,  0,-10,
+     -5,  0,  5,  5,  5,  5,  0, -5,
+      0,  0,  5,  5,  5,  5,  0, -5,
+    -10,  5,  5,  5,  5,  5,  0,-10,
+    -10,  0,  5,  0,  0,  0,  0,-10,
+    -20,-10,-10, -5, -5,-10,-10,-20
+};
+
+// King (midgame only)
+static constexpr int king_table[64] = {
+    -30,-40,-40,-50,-50,-40,-40,-30,
+    -30,-40,-40,-50,-50,-40,-40,-30,
+    -30,-40,-40,-50,-50,-40,-40,-30,
+    -30,-40,-40,-50,-50,-40,-40,-30,
+    -20,-30,-30,-40,-40,-30,-30,-20,
+    -10,-20,-20,-20,-20,-20,-20,-10,
+     20, 20,  0,  0,  0,  0, 20, 20,
+     20, 30, 10,  0,  0, 10, 30, 20
+};
+
+int psqt_score(const Board& b) {
+
+    int score = 0;
+
+    const int* tables[6] = {
+        pawn_table,
+        knight_table,
+        bishop_table,
+        rook_table,
+        queen_table,
+        king_table
+    };
+
+    for (int piece = PAWN; piece <= KING; piece++) {
+
+        // WHITE
+        Bitboard bb = b.bb_pieces[WHITE][piece];
+        while (bb) {
+            int sq = __builtin_ctzll(bb);
+            score += tables[piece][sq];
+            bb &= bb - 1;
+        }
+
+        // BLACK
+        bb = b.bb_pieces[BLACK][piece];
+        while (bb) {
+            int sq = __builtin_ctzll(bb);
+            score -= tables[piece][mirror_square(sq)];
+            bb &= bb - 1;
+        }
+    }
+
+    return score;
+}
---
+++ src/engine/psqt.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#include "board.h"
+
+int psqt_score(const Board& b);
